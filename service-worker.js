71ea6c16c9382b28dbe8d9d886aa940c04be8415
dselman <danscode@selman.org>
2020-05-02 15:22:05 +0000

    Deploying to gh-pages from  @ 80b5f1ffbf6ddaa57875ce457a0952725ec57810 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.47e0880b4abcbfccdf1e2c21c9d8e727.js"
+  "precache-manifest.7a4efe8cc242c86d288d287ef1c1756b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
