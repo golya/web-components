6c0c32bada2ef2e43660fe0bfd16469a53aa59eb
jeromesimeon <jeromesimeon@users.noreply.github.com>
2020-05-11 21:36:39 +0000

    Deploying to gh-pages from  @ 1a2bf3591fb0bd56ae3a6a98f9205f0de8d17c9e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.bc8b9289b0c5099a1a9421448b688fc1.js"
+  "precache-manifest.03cd47b45cefccd1057e236e35ac0cea.js"
 );
 
 self.addEventListener('message', (event) => {
