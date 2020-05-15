59c5a1f4fa496dee2e91ef248d608e96c0545660
DianaLease <DianaRLease@gmail.com>
2020-05-15 17:29:20 +0000

    Deploying to gh-pages from  @ 7cfa66a78efdcb9d34eaf454c7b8b7bbef44d6ce 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.05da81c566050b74eabcac40aa812eec.js"
+  "precache-manifest.68e8f71576e8f7b05980520dee18bd42.js"
 );
 
 self.addEventListener('message', (event) => {
